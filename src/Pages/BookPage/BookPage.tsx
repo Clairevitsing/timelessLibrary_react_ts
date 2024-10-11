13fea69128da5b09f3d4ae2385fafd3be701bfe7
claire <vitsingz@gmail.com>
2024-10-11 16:41:50 +0200

    correct the style of bookpage

--- src/Pages/BookPage/BookPage.tsx
+++ src/Pages/BookPage/BookPage.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 import { fetchBooks } from '../../Services/BookService';
 import { Book } from '../../Models/Book';
-import { useNavigate } from 'react-router-dom';  
+import { useNavigate } from 'react-router-dom'; 
+import './BookPage.css';
 
-const BookPage: React.FC = () => {
+const BookPage = () => {
     const [books, setBooks] = useState<Book[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState('');
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetchBooks().then(books => {
@@ -28,30 +30,44 @@ const BookPage: React.FC = () => {
          navigate(`/book/${id}`); 
     };
 
+    if (!books.length) {
+    return <p>No books found!</p>; 
+  }
+
     return (
-        <div className="container mt-4">
-            <h2>All the Books</h2>
-            <div className="row">
-                {books.map(book => (
-                    <div key={book.id} className="col-sm-6 col-md-4 col-lg-3 mb-3">
-                        <div className="card">
-                            <img src={book.image} className="card-img-top" alt={book.title} />
-                            <div className="card-body">
-                                <h5 className="card-title">{book.title}</h5>
-                                {/* <p className="card-text">{book.description}</p> */}
-                                <ul>
-                                    {book.authors.map(author => (
-                                        <li key={author.id}>{author.firstName} {author.lastName}</li>
-                                    ))}
-                                </ul>
-                                 <button className="btn btn-primary" onClick={() => handleDetailsClick(book.id)}>Details</button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+    <>
+     <div className="search-container">
+        <Form className="search-form d-flex">
+            <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+            <Button variant="outline-success" type="submit">Search</Button>
+        </Form>
+    </div>
+    <div className="addBook">
+            <Button variant="outline-success" type="submit">Add new book</Button>
+    </div> 
+      <div className="container mt-4">
+        <h2>All the Books</h2>
+        <div className="row">
+          {books.map(book => (
+            <div key={book.id} className="col-sm-6 col-md-4 col-lg-3 mb-3">
+              <div className="card">
+                <img src={book.image || 'default-book-image.jpg'} className="card-img-top" alt={book.title} />
+                <div className="card-body">
+                  <h5 className="card-title">{book.title}</h5>
+                  <ul>
+                    {book.authors.map(author => (
+                      <li key={author.id}>{author.firstName} {author.lastName}</li>
+                    ))}
+                  </ul>
+                  <button className="btn btn-primary" onClick={() => handleDetailsClick(book.id)}>Details</button>
+                </div>
+              </div>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default BookPage;
